1fbf46ef93af35cb68ecb3f1e63c3ab4ea1f6616
masahiro-g <kwoaudis32@gmail.com>
2019-03-08 11:46:56 +0900

    今日の運勢機能 実装

--- app/controllers/fortunetelling_controller.rb
+++ app/controllers/fortunetelling_controller.rb
@@ -3,6 +3,7 @@ class FortunetellingController < ApplicationController
     @years = params[:numbers][0..3].to_i
     @months = params[:numbers][4..5].to_i
     @dates = params[:numbers][6..7].to_i
-    # @age = params[:numbers].to_i
+    @fortune = ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle[0]
+
   end
 end
