43b4afa7dea3be36623a5c9fe3efc8f6360cd288
masahiro-g <kwoaudis32@gmail.com>
2019-03-14 18:28:57 +0900

    if文の修正､date -> day

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,14 +1,14 @@
-class FortunetellingController < ApplicationController
-  include FortunetellingHelper
+class FortunetellingsController < ApplicationController
+  include FortunetellingsHelper
 
   def show
     @birthday = params[:birthday]
     #@birthdayにパラメーターを入れたものに､それぞれ代入する｡
-    @year = @birthday[0, 4].to_i
-    @month = @birthday[4, 2].to_i
-    @date = @birthday[6, 2].to_i
+    @year = @birthday[0, 4]
+    @month = @birthday[4, 2]
+    @day = @birthday[6, 2]
     @happy = happy(@birthday)
-    @constellations = constellation(@month, @date)
+    @constellations = constellation(@month, @day)
     @fortune = ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle[0]
     @age = age(@birthday)
   end
